5c2f34015000e6eefae99a56ae33ce184e113c15
Osama Basheer <838@holbertonschool.com>
2019-07-27 22:09:01 +0000

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _printf - is a function that formats and prints data
+ * @:
+ * Return:
+ */
+int _printf(const char *format, ...)
+{
+	va_list list;
+
+	va_start;
+
+	va_end;
+}
