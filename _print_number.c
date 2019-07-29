3f915b607f85ff3d1d71e14e365099da687df706
Gareth Brickman <977@holbertonschool.com>
2019-07-29 21:48:03 +0000

    Edit _print_number for if...else if error again

--- _print_number.c
+++ _print_number.c
@@ -12,20 +12,23 @@ void _print_number(int n, int *k)
 	int y;
 
 	if (n == 0)
+	{
 		_putchar(((n / y) + '0'), k);
-
-	else if (n < 0)
+	}
+	if (n < 0)
 		n = n * (-1);
 	for (y = 1000000000; y > 0; y = y / 10)
 	{
 		if (n / y != 0)
 			_putchar(('-'), k);
-			_putchar(((n / y) % 10 + '0'), k);
+		_putchar(((n / y) % 10 + '0'), k);
 	}
 	else
+	{
 		for (y = 1000000000; y > 0; y = y / 10)
 		{
 			if (n / y != 0)
 				_putchar(((n / y) % 10 + '0'), k);
 		}
+	}
 }
