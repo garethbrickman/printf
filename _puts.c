b1d1fd9428b414d0456219764c154a0c2b72379d
Osama Basheer <838@holbertonschool.com>
2019-08-01 01:28:42 +0000

    Fix Checker Issues

--- _puts.c
+++ _puts.c
@@ -7,6 +7,16 @@
  */
 void _puts(char *str, int *k)
 {
+	if (!str)
+	{
+		_putchar('(', k);
+		_putchar('n', k);
+		_putchar('u', k);
+		_putchar('l', k);
+		_putchar('l', k);
+		_putchar(')', k);
+		return;
+	}
 	while (*str)
 	{
 		_putchar(*str, k);
